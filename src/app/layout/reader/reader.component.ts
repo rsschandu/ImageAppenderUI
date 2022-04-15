f2d00ab49a8e2c8e603a3cb576f1091fe7cc4209
rsschandu <rsschandu212@gmail.com>
2022-04-16 00:41:03 +0530

    till modal

--- src/app/layout/reader/reader.component.ts
+++ src/app/layout/reader/reader.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ReaderService } from './reader.service';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-reader',
@@ -7,14 +8,40 @@ import { ReaderService } from './reader.service';
   styleUrls: ['./reader.component.scss']
 })
 export class ReaderComponent implements OnInit {
+  closeResult: string;
 
-  constructor(private readerService: ReaderService) { }
+  constructor(private readerService: ReaderService,private modalService: NgbModal) { }
 
   pageData: any;
   ngOnInit(): void {
+    this.getPageData();
+  }
+
+  getPageData() {
     this.readerService.getData().subscribe(data => {
       this.pageData = data;
     })
   }
+
+  open(content) {
+    this.modalService.open(content).result.then(
+        (result) => {
+            this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+    );
+}
+
+private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+    } else {
+        return `with: ${reason}`;
+    }
+}
 
 }

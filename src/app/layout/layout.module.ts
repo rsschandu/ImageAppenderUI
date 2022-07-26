1d1264b19b0af03a4903b1569b7fedf0d07c35f8
rsschandu <rsschandu212@gmail.com>
2022-07-26 20:10:26 +0530

    local changes temp

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -9,9 +9,10 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { FormsModule } from '@angular/forms';
 import { TagsComponent } from './tags/tags.component';
+import { TempComponent } from './temp/temp.component';
 
 @NgModule({
     imports: [CommonModule, LayoutRoutingModule, TranslateModule, NgbDropdownModule, FormsModule],
-    declarations: [LayoutComponent, SidebarComponent, HeaderComponent, TagsComponent]
+    declarations: [LayoutComponent, SidebarComponent, HeaderComponent, TagsComponent, TempComponent]
 })
 export class LayoutModule {}
